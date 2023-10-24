4589f64351f21a2364d29dc06a2f5cc07158cfa8
ghosind <ghosind@gmail.com>
2023-10-24 22:37:08 +0800

    feat: add All and AllWithContext.

---
+++ all.go
@@ -0,0 +1,64 @@
+package async
+
+import (
+	"context"
+	"errors"
+)
+
+func All(funcs ...func(context.Context) error) error {
+	return allWithContext(context.Background(), funcs...)
+}
+
+func AllWithContext(ctx context.Context, funcs ...func(context.Context) error) error {
+	return allWithContext(ctx, funcs...)
+}
+
+func allWithContext(parent context.Context, funcs ...func(context.Context) error) error {
+	if len(funcs) == 0 {
+		return nil
+	}
+
+	ctx, canFunc := context.WithCancel(parent)
+	errCh := make(chan error)
+	retCh := make(chan struct{}, len(funcs))
+
+	defer canFunc()
+	defer close(errCh)
+	defer close(retCh)
+
+	for i := 0; i < len(funcs); i++ {
+		fn := funcs[i]
+		go func() {
+			childCtx, childCanFunc := context.WithCancel(ctx)
+			defer childCanFunc()
+
+			err := fn(childCtx)
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				if err != nil {
+					errCh <- err
+				} else {
+					retCh <- struct{}{}
+				}
+			}
+		}()
+	}
+
+	finished := 0
+	for {
+		select {
+		case <-parent.Done():
+			return errors.New("context canceled")
+		case err := <-errCh:
+			return err
+		case <-retCh:
+			finished++
+		}
+
+		if finished == len(funcs) {
+			return nil
+		}
+	}
+}
---
+++ all_test.go
@@ -0,0 +1,83 @@
+package async_test
+
+import (
+	"context"
+	"errors"
+	"testing"
+	"time"
+
+	"github.com/ghosind/go-assert"
+	"github.com/ghosind/go-async"
+)
+
+func TestAllWithoutFuncs(t *testing.T) {
+	a := assert.New(t)
+
+	err := async.All()
+	a.NilNow(err)
+}
+
+func TestAllSuccess(t *testing.T) {
+	a := assert.New(t)
+
+	data := make([]bool, 5)
+	funcs := make([]func(context.Context) error, 0, 5)
+	for i := 0; i < 5; i++ {
+		n := i
+		funcs = append(funcs, func(ctx context.Context) error {
+			time.Sleep(time.Duration(n*100) * time.Millisecond)
+			data[n] = true
+			return nil
+		})
+	}
+
+	err := async.All(funcs...)
+	a.NilNow(err)
+	a.EqualNow(data, []bool{true, true, true, true, true})
+}
+
+func TestAllFailure(t *testing.T) {
+	a := assert.New(t)
+
+	data := make([]bool, 5)
+	funcs := make([]func(context.Context) error, 0, 5)
+	for i := 0; i < 5; i++ {
+		n := i
+		funcs = append(funcs, func(ctx context.Context) error {
+			time.Sleep(time.Duration(n*100) * time.Millisecond)
+			if n == 2 {
+				return errors.New("n = 2")
+			}
+			data[n] = true
+			return nil
+		})
+	}
+
+	err := async.All(funcs...)
+	a.NotNilNow(err)
+	a.EqualNow(err.Error(), "n = 2")
+	a.EqualNow(data, []bool{true, true, false, false, false})
+}
+
+func TestAllWithTimeoutedContext(t *testing.T) {
+	a := assert.New(t)
+
+	data := make([]bool, 5)
+	funcs := make([]func(context.Context) error, 0, 5)
+	for i := 0; i < 5; i++ {
+		n := i
+		funcs = append(funcs, func(ctx context.Context) error {
+			time.Sleep(time.Duration(n*100) * time.Millisecond)
+			data[n] = true
+			return nil
+		})
+	}
+
+	ctx, canFunc := context.WithTimeout(context.Background(), 150*time.Millisecond)
+	defer canFunc()
+
+	err := async.AllWithContext(ctx, funcs...)
+	a.NotNilNow(err)
+	a.Equal(err.Error(), "context canceled")
+	a.EqualNow(data, []bool{true, true, false, false, false})
+}
