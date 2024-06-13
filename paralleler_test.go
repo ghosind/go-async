736e568b9b653375d8e025b0d019a7f05f653d1f
ghosind <ghosind@gmail.com>
2024-06-13 20:38:18 +0800

    test: more test cases for Paralleler.

--- paralleler_test.go
+++ paralleler_test.go
@@ -25,8 +25,44 @@ func TestParalleler(t *testing.T) {
 
 	a.EqualNow(cnt.Load(), 0)
 	_, err := p.Run()
-	a.Nil(err)
+	a.NilNow(err)
 	a.EqualNow(cnt.Load(), 5)
+}
+
+func TestParallelerWithoutTasks(t *testing.T) {
+	a := assert.New(t)
+	p := new(async.Paralleler)
+
+	out, err := p.Run()
+	a.NilNow(err)
+	a.EqualNow(out, [][]any{})
+
+	out, err = p.RunCompleted()
+	a.NilNow(err)
+	a.EqualNow(out, [][]any{})
+}
+
+func TestParallelerRunWithFailure(t *testing.T) {
+	a := assert.New(t)
+	cnt := atomic.Int32{}
+	expectedErr := errors.New("n = 2")
+
+	p := new(async.Paralleler).WithConcurrency(1)
+	for i := 0; i < 5; i++ {
+		n := i
+		p.Add(func() error {
+			cnt.Add(1)
+			if n == 2 {
+				return expectedErr
+			}
+			return nil
+		})
+	}
+
+	out, err := p.Run()
+	a.NotNilNow(err)
+	a.EqualNow(out, [][]any{{nil}, {nil}, {expectedErr}, {}, {}})
+	a.EqualNow(cnt.Load(), 3)
 }
 
 func TestParallelerAddTasks(t *testing.T) {
@@ -41,7 +77,7 @@ func TestParallelerAddTasks(t *testing.T) {
 	}
 
 	_, err := p.Run()
-	a.Nil(err)
+	a.NilNow(err)
 	a.EqualNow(cnt.Load(), 5)
 
 	for i := 0; i < 3; i++ {
@@ -108,7 +144,7 @@ func TestParallelerWithContext(t *testing.T) {
 	ctx, canFunc := context.WithTimeout(context.Background(), 80*time.Millisecond)
 	defer canFunc()
 
-	p := new(async.Paralleler).WithConcurrency(2).WithContext(ctx)
+	p := new(async.Paralleler).WithConcurrency(1).WithContext(ctx)
 	for i := 0; i < 5; i++ {
 		p.Add(func(ctx context.Context) {
 			select {
@@ -123,10 +159,28 @@ func TestParallelerWithContext(t *testing.T) {
 
 	_, err := p.Run()
 	a.EqualNow(err, async.ErrContextCanceled)
-	a.EqualNow(cnt.Load(), 2)
+	a.EqualNow(cnt.Load(), 1)
 }
 
 func TestParallelerRunCompleted(t *testing.T) {
+	a := assert.New(t)
+	cnt := atomic.Int32{}
+
+	p := new(async.Paralleler).WithConcurrency(2)
+	for i := 0; i < 5; i++ {
+		p.Add(func() error {
+			cnt.Add(1)
+			return nil
+		})
+	}
+
+	out, err := p.RunCompleted()
+	a.NilNow(err)
+	a.EqualNow(out, [][]any{{nil}, {nil}, {nil}, {nil}, {nil}})
+	a.EqualNow(cnt.Load(), 5)
+}
+
+func TestParallelerRunCompletedWithFailure(t *testing.T) {
 	a := assert.New(t)
 	cnt := atomic.Int32{}
 	expectedErr := errors.New("n = 2")
